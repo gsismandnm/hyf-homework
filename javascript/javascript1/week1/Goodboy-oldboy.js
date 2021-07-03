f9136b7c7c4700a3ebfbb68c7fc52905648f7328
gsismandnm <gknsismandenmark@gmail.com>
2021-07-03 13:35:46 +0200

    Goodboy-oldboy.js

--- javascript/javascript1/week1/Goodboy-oldboy.js
+++ javascript/javascript1/week1/Goodboy-oldboy.js
@@ -1,10 +1,10 @@
 var dogYearOfBirth = 2020;
 var dogYearFuture = 2027;
-var shouldShowResultInDogYear = dogYearOfFuture - dogYearOfBirth;
-var dogYear = dogHumanYear * 7;
+var shouldShowResultInDogYears = dogYearOfFuture - dogYearOfBirth;
+var shouldShowResultInHumanYears = dogYears * 7;
 var shouldShowResultInDogYear = true;
 if (shouldShowResultInDogYear) {
-    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture + ".")
+    console.log("Your dog will be " + dogYears + " dog years old in " + dogYearFuture + ".")
 } else {
-    console.log("Your dog will be " + HumanYear + " human years old in " + dogYearFuture + ".")
+    console.log("Your dog will be " + HumanYears + " human years old in " + dogYearFuture + ".")
 }
